2ecceccfd8fe19822129930d98f77d992ba8bca8
ThDarKSouL <anuraggaonkar36@gmail.com>
2024-12-30 17:05:49 +0530

    second-commit

---
+++ src/main/java/com/enterprise/ems/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.enterprise.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
